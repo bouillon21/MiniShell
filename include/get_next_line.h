801eb40a960e067ec2d1b65852bd41b345f429bd
Bulat <b.barzhanov@yandex.ru>
2021-03-21 16:10:29 +0300

    fix header gnl

--- include/get_next_line.h
+++ include/get_next_line.h
@@ -1,21 +1,10 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: hmickey <hmickey@student.42.fr>            +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/11/11 12:58:59 by cshelli           #+#    #+#             */
-/*   Updated: 2021/03/21 13:56:44 by hmickey          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # include <stdlib.h>
 # include <unistd.h>
 # include <fcntl.h>
 # include <stdio.h>
+#include "libft.h"
 # define BUFFER_SIZE 32
 
 int		get_next_line(int fd, char **line);
