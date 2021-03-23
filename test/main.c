9dd8e7f293aa9e9c6aeb95d6a4916047859c9e21
ZectorJay <zKimiko@yandex.ru>
2021-03-23 18:30:21 +0300

    testing termcap

--- test/main.c
+++ test/main.c
@@ -6,11 +6,25 @@
 /*   By: hmickey <hmickey@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/23 13:17:10 by hmickey           #+#    #+#             */
-/*   Updated: 2021/03/23 16:51:11 by hmickey          ###   ########.fr       */
+/*   Updated: 2021/03/23 18:16:34 by hmickey          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "test.h"
+
+static int get_capability(const char* label, const char* name, const char** pptr, char** p_buf_ptr)
+{
+	const char* ptr;
+
+	ptr = tgetstr(name, p_buf_ptr);
+
+	printf("%-22s (%s) = ", label, name);
+	print_text(ptr);
+	printf("\n");
+
+	*pptr = ptr;
+	return ptr != NULL;
+}
 
 int main(int argc, char **argv, char **envp)
 {
@@ -18,23 +32,13 @@ int main(int argc, char **argv, char **envp)
 
 	term.termtype = getenv("minishell");
 	term.tgetent = tgetent(term.termbuf, term.termtype);
-	// while (1)
-	// {
-	// if (key_enter == 1)
-		printf("%s\n", "PIZDA");
-	// }
-	int ent;
 	lines   = tgetnum("li");
 	columns = tgetnum("co");
-	while ((ent = tgetnum("@8") < 0))
-	{
-		sleep(1);
-		printf("pizda\n");
-	}
+
+	
 	
 	printf("lines   (li) = %d\n", lines);	// высота терминала
 	printf("columns   (co) = %d\n", columns);	 // ширина терминала
-	printf("enter   (en) = %d\n", ent);	 // ширина терминала
 }
 
 
