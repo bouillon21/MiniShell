5c63e1f2d2c949fe729b15f54521fbe944d9434b
ZectorJay <zKimiko@yandex.ru>
2021-03-30 15:30:40 +0300

    changing parse logic

--- test/srcs/parsing/parse_input.c
+++ test/srcs/parsing/parse_input.c
@@ -6,46 +6,15 @@
 /*   By: hmickey <hmickey@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/28 14:58:24 by hmickey           #+#    #+#             */
-/*   Updated: 2021/03/30 14:59:15 by hmickey          ###   ########.fr       */
+/*   Updated: 2021/03/30 15:30:40 by hmickey          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../test.h"
 
-int		search_quote_close(int i, char *symbol)
-{
-	while (g_string[i] != *symbol && g_string[i])
-	{
-		i++;
-		if (g_string[i] == *symbol && g_string[i - 1] == '\\')
-			i++;
-		else if (g_string[i] == *symbol)
-			return (1);
-	}
-	return(0);			// ВМЕСТО ЭТОГО СДЕЛАТЬ ERROR ФУНКЦИЮ;
-}
-
 int		search_command(t_all *all, int start)
 {
-	int		i;
-	char	*tmp;
-	char	*symb;
-
-	i = start;
-	while (g_string[i] != ' ' && g_string[i] != '\n' && g_string[i] != ';')
-	{
-		if (g_string[i] == '\'' || g_string[i] == '\"')
-		{
-			symb = ft_strrchr("\'\"", g_string[i]);
-			if (!search_quote_close(++i, symb))
-				;
-		}
-		i++;
-	}
-	tmp = ft_substr(g_string, start, i - start);
-	//checking and etc;
-	all->token->command = tmp;
-	return(i);
+	
 }
 
 int		search_flags(t_all *all, int start)
@@ -63,11 +32,9 @@ void	parse_string(t_all *all)
 	while (g_string[i] && g_string[i] != '\n')
 	{
 		i = search_command(all, i);
-		write(1, all->token->command, ft_strlen(all->token->command));
-		write(1, "\n", 1);
 		i = search_flags(all, i);
+		i = search_args(all, i);
 		all->token->next = create_new_token(all->token);
 		all->token = all->token->next;
-		i++;
 	}
 }
